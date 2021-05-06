ac9c93d8e31bbd079453d161e80e0e124e49c827
SmaranikaB05 <49447726+SmaranikaB05@users.noreply.github.com>
2021-05-06 22:35:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html> 
 	<head> 
-	<title>PHP Smaranika Test- Checking 3rd round</title>
+	<title>PHP Smaranika Test- Checking 5th round</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
 	<h1>PHP 1663Test</h1>
-		<p><b>An Example for  Smaranika ` PHP in Action</b></p>
+		<p><b>An Example for  Smaranika - 5th round` PHP in Action</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION2 </h2> 
